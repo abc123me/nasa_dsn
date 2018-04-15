4bcec638b0cab2e15bcfe1a4af7fd0a73da29fe7
jeremiah_abc123me <jeremiah_abc123me@hotmail.com>
2018-04-15 15:09:27 -0400

    Modified adlib.py and testing.py, also used testing.py to test things

--- adlib.py
+++ adlib.py
@@ -1,3 +1,13 @@
+from smbus import SMBus
+#Primitives for manally setting up the ADC
+def i2cInit(i2cID):
+    return SMBus(i2cID)
+def adInit(bus):
+    bus.write_byte(0x35, 0x80)
+    bus.write_byte(0x35, 0x01)    
+def readAD(bus):
+    buffer = bus.read_i2c_block_data(0x40, 0xe3, 2)
+    return 256 * int(buffer[1]) + int(buffer[0])
 #Object oriented form as well, because WHY NOT?
 class ADC:
     def __init__(self, i2cBusID):
@@ -6,20 +16,12 @@ class ADC:
         adInit(self.i2cBus)
     def getValue(self):
         return readAD(self.i2cBus)
-    
-#Primitives for manally setting up the ADC
-def i2cInit(i2cID):
-    return smbus.SMBus(i2cID)
-def adInit(bus):
-    bus.write_byte(0x35, 0x80)
-    bus.write_byte(0x35, 0x01)    
-def readAD(bus):
-    buffer = bus.read_i2c_block_data(0x40, 0xe3, 2)
-    return 256 * int(buffer[1]) + int(buffer[0])
+    def getI2CBus(self):
+        return self.i2cBus
 
 #### Original code for reference
 ##Set up I2C
-#bus=smbus.SMBus(0)
+#bus=SMBus(0)
 #bus.write_byte(0x35,0x80)
 #bus.write_byte(0x35,0x01)
 #buffer=bus.read_i2c_block_data(0x40,0xe3,2)
--- testing.py
+++ testing.py
@@ -1,10 +1,5 @@
-def decibelsToBinary(num):
-	val = [False] * 8
-	for i in range(7, -1, -1):
-		mdbv = 2 ** (i - 2)
-		if(num >= mdbv):
-			num = num - mdbv;
-			val[i] = True
-			print("num " + str(num) + " > " + str(mdbv))
-	return val
-print(decibelsToBinary(10.25))
+def a():
+	return "a"
+def testFunc(func):
+	return func()
+print(testFunc(a))
